31e6d6bde51823e6a4c1b345985bcb7273cdc23a
Rajdeep <rajdeeppilla@gmail.com>
2021-04-12 14:29:26 +0530

    added exceptionhandling

--- spring-boot-H2/src/main/java/com/rajdeep/booth2/repository/CourseRepository.java
+++ spring-boot-H2/src/main/java/com/rajdeep/booth2/repository/CourseRepository.java
@@ -1,5 +1,7 @@
 package com.rajdeep.booth2.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.rajdeep.booth2.entity.Course;
--- spring-boot-H2/src/main/java/com/rajdeep/booth2/service/CourseService.java
+++ spring-boot-H2/src/main/java/com/rajdeep/booth2/service/CourseService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.rajdeep.booth2.ResourceNotFoundException;
 import com.rajdeep.booth2.entity.Course;
 import com.rajdeep.booth2.repository.CourseRepository;
 
@@ -20,6 +21,11 @@ public class CourseService {
 
 	public List<Course> getCourses() {
 		return courseRepository.findAll();
+	}
+
+	public Course getCourseById(int id) {
+		return courseRepository.findById(id).orElseThrow(
+				() -> new ResourceNotFoundException("Employee not found" + id));
 	}
 	
 }
